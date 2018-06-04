3d6ad3dddc73df1c1b44e9fbdf4ff0bd501d2c77
thisisaaronland <straup@gmail.com>
2018-06-04 13:49:34 -0700

    block out Closest colour code

--- palette/palette.go
+++ palette/palette.go
@@ -16,3 +16,28 @@ type Color struct {
 	Color   string `json:"color"`
 	Closest string `json:"closest,omitempty"`
 }
+
+func Closest(hex_color string, possible []PColor) PColor {
+
+     r, g,b, _ := hexcolor.HexToRGBA(hex)
+
+     lookup := make(map[int]PColor)
+     keys := make([]int, 0)
+
+     for _, c := range possible {
+
+     	 rc, gc, bc, _ := hexcolor.HexToRGBA(c.Hex)
+
+	 rd := (rc - r) ** 2
+	 gd := (gc - g) ** 2
+	 bd := (bc - b) ** 2
+
+	 k := rd + gd + bd
+	 lookup[k] = c
+
+	 keys = append(keys, k)
+     }
+
+     sort.Ints(keys)
+     return lookup[keys[0]]
+}
