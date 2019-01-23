6b9e9c0cccff5358cf22bf5de63b86be42f7c260
thisisaaronland <devnull@localhost>
2019-01-22 17:57:38 -0800

    remove unnecessary files

--- docker/server/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# This is copied in to the built Dockerfile by "docker build -t go-iiif ."
-# It exists principally so we can configure ${IIIF_SERVER_CONFIG} dynamically
-# (20180620/thisisaaronland)
-
-/bin/iiif-server -host 0.0.0.0 -config ${IIIF_SERVER_CONFIG}
