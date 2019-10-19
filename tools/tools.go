8fe09c8e0dfce61228b654e88d19f6ff1eef1adf
thisisaaronland <devnull@localhost>
2019-10-18 17:25:40 -0700

    notes

--- tools/tools.go
+++ tools/tools.go
@@ -1,5 +1,23 @@
 package tools
 
+/*
+
+the idea for each and any of the non-native drivers to be able to provide their
+own copy of the standard toolset by doing something like this (error handling
+omitted for brevity) :
+
+import (
+	_ "github.com/go-iiif/go-iiif/native"
+	"github.com/go-iiif/go-iiif/tools"
+)
+
+func main() {
+	tool, _ := tools.NewTransformTool()
+	tool.Run()
+}
+
+*/
+
 type Tool interface {
 	Run() error
 }
