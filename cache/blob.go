bddfa79c8b3d922719801bc76ccab4a4d6dea392
thisisaaronland <devnull@localhost>
2019-10-18 18:31:15 -0700

    stub out blob cache

---
+++ cache/blob.go
@@ -0,0 +1,84 @@
+package cache
+
+import (
+	"context"
+	iiifconfig "github.com/go-iiif/go-iiif/config"
+	"github.com/aaronland/gocloud-blob-bucket"
+	"gocloud.dev/blob"
+	"io/ioutil"
+)
+
+type BlobCache struct {
+	Cache
+	bucket *blob.Bucket
+}
+
+func NewBlobCache(config *iiifconfig.Config) (Cache, error) {
+
+	cfg := config.Images
+	ctx := context.Background()
+	
+	b, err := bucket.OpenBucket(ctx, cfg.Cache.Path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	bc := &BlobCache{
+		bucket: b,
+	}
+
+	return bc, nil
+}
+
+func (bc *BlobCache) Exists(uri string) bool {
+
+	ctx := context.Background()
+
+	exists, err := bc.bucket.Exists(ctx, uri)
+
+	if err != nil {
+		return false
+	}
+
+	return exists
+}
+
+func (bc *BlobCache) Get(uri string) ([]byte, error) {
+
+	ctx := context.Background()
+
+	fh, err := bc.bucket.NewReader(ctx, uri, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer fh.Close()
+
+	return ioutil.ReadAll(fh)
+}
+
+func (bc *BlobCache) Set(uri string, body []byte) error {
+
+	ctx := context.Background()
+
+	fh, err := bc.bucket.NewWriter(ctx, uri, nil)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = fh.Write(body)
+
+	if err != nil {
+		return err
+	}
+
+	return fh.Close()
+}
+
+func (bc *BlobCache) Unset(uri string) error {
+	ctx := context.Background()
+	return bc.bucket.Delete(ctx, uri)
+}
