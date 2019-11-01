dfd2e8bf81efd3756b4c9f16ae4c7d90ac02e3f5
thisisaaronland <devnull@localhost>
2019-10-31 17:07:21 -0700

    update to use go-iiif-uri 0.1.4

--- vendor/github.com/go-iiif/go-iiif-uri/uri.go
+++ vendor/github.com/go-iiif/go-iiif-uri/uri.go
@@ -1,7 +1,11 @@
 package uri
 
 import (
+	"errors"
+	"fmt"
+	"log"
 	"net/url"
+	"regexp"
 )
 
 type URI interface {
@@ -13,5 +17,24 @@ type URI interface {
 
 func NewURI(str_uri string) (URI, error) {
 
-	return NewURIWithDriver(str_uri)
+	u, err := NewURIWithDriver(str_uri)
+
+	if err == nil {
+		return u, nil
+	}
+
+	re, re_err := regexp.Compile(`^\w+\:\/\/`)
+
+	if re_err != nil {
+		return nil, re_err
+	}
+
+	if re.MatchString(str_uri) {
+		return nil, errors.New("Invalid or unsupported URI string")
+	}
+
+	file_uri := fmt.Sprintf("%s:///%s", FileDriverName, str_uri)
+	log.Println(str_uri, file_uri)
+
+	return NewURIWithDriver(file_uri)
 }
