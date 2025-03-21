182d2e5e5aa5640da798793340b4b0934f0de2ec
thisisaaronland <thisisaaronland@localhost>
2025-03-20 17:56:44 -0700

    report.html

--- process/report.go
+++ process/report.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"context"
 	"crypto/sha256"
+	_ "embed"
 	"fmt"
 	"path/filepath"
 	"sort"
@@ -12,10 +13,12 @@ import (
 	"html/template"
 	
 	iiifuri "github.com/go-iiif/go-iiif-uri"
-	// "github.com/go-iiif/go-iiif/v6/static/templates/html"
 	"github.com/jtacoma/uritemplates"
 	"github.com/tidwall/gjson"
 )
+
+//go:embed report.html
+var report_html string
 
 const REPORTNAME_TEMPLATE string = "process_{sha256_origin}.json"
 
@@ -84,20 +87,11 @@ func GenerateProcessReportHTML(ctx context.Context, report_body []byte) ([]byte,
 		Images []*Image
 	}
 
-	var t *template.Template
-
-	/*
-	t, err := html.LoadTemplates(ctx)
-
+	t := template.New("report")
+	t, err := t.Parse(report_html)
+	
 	if err != nil {
 		return nil, err
-	}
-	*/
-	
-	t = t.Lookup("process_report")
-
-	if t == nil {
-		return nil, fmt.Errorf("Missing 'process_report' template")
 	}
 
 	vars := HTMLVars{
@@ -107,7 +101,7 @@ func GenerateProcessReportHTML(ctx context.Context, report_body []byte) ([]byte,
 	var buf bytes.Buffer
 	wr := bufio.NewWriter(&buf)
 
-	err := t.Execute(wr, vars)
+	err = t.Execute(wr, vars)
 
 	if err != nil {
 		return nil, fmt.Errorf("Failed to render 'process_report' template, %w", err)
